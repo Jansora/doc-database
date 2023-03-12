062b9d8b5046f903be75efc8878f6c5c76340eca
Jansora <zhangyue1936@gmail.com>
2023-03-12 21:40:01 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,39 +1,82 @@
-export default {
-    title: 'Doc Template',
-    titleTemplate: 'Jansora\'s knowledge base',
-    description: 'Just playing around.',
-    cleanUrls: true,
-    lang: 'zh-CN',
-    lastUpdated: true,
-    themeConfig: {
-        nav: [
-            { text: 'Guide', link: '/guide' },
-            {
-                text: 'Dropdown Menu',
-                items: [
-                    { text: 'Item A', link: '/item-1' },
-                    { text: 'Item B', link: '/item-2' },
-                    { text: 'Item C', link: '/item-3' }
-                ]
-            }
-        ],
-        sidebar: [
-            {
-                text: 'Guide',
-                items: [
-                    { text: 'Introduction', link: '/introduction' },
-                    { text: 'Getting Started', link: '/getting-started' },
 
-                ]
-            }
+function sidebarMysql() {
+    return [
+        {
+            text: '组件商城(2023.03.09)', collapsed: false, items: [
+                { text: '部署手册', link: '/mysql/honeycomb-store/deploy' },
+                { text: '建表脚本', link: '/project/honeycomb-store/ddl' },
+                { text: '接口文档', link: '/project/honeycomb-store/api' },
+            ]
+        },
+        {
+            text: '日志收集工程(2022.12.01)', collapsed: false, items: [
+                { text: '部署手册', link: '/project/log-collection/deploy' },
+                // { text: '建表脚本', link: '/project/honeycomb-store/ddl' },
+                // { text: '接口文档', link: '/project/honeycomb-store/api' },
+            ]
+        },
+        {
+            text: '啄木鸟监控(2022.10.16)', collapsed: false, items: [
+                { text: '部署手册', link: '/project/woodpecker/deploy' },
+                // { text: '建表脚本', link: '/project/honeycomb-store/ddl' },
+                // { text: '接口文档', link: '/project/honeycomb-store/api' },
+            ]
+        },
+    ]
+}
+
+function sidebarPostgres() {
+    return [
+        {
+            text: 'Postgres Spring 实践', collapsed: false, items: [
+                { text: '数组字段映射', link: '/postgres/spring/array-field' },
+            ]
+        },
+    ]
+}
+
+export default {
+    title: "Jansora's document",
+        titleTemplate: 'Jansora\'s knowledge base',
+        description: 'Just playing around.',
+        cleanUrls: true,
+        lang: 'zh-CN',
+        lastUpdated: true,
+        themeConfig: {
+        nav: [
+            // { text: 'project', link: '/project' },
+            // { text: 'project', link: '/project' },
+            { text: '项目管理', link: '/project/honeycomb-store/deploy' },
+            { text: '研发指南', link: '/development/qa/api-doc' },
+            { text: '资源索引', link: '/linked/development/1' },
+            { text: '新人必读', link: '/new-comers/first-day' },
+            // {
+            //     text: 'Dropdown Menu',
+            //     items: [
+            //         { text: 'Item A', link: '/item-1' },
+            //         { text: 'Item B', link: '/item-2' },
+            //         { text: 'Item C', link: '/item-3' }
+            //     ]
+            // }
         ],
+        sidebar: {
+
+            // '/mysql/': sidebarMysql(),
+            '/postgres/': sidebarPostgres(),
+                // '/linked/': sidebarLinked(),
+                // '/new-comers/': sidebarNewComers(),
+        }
+
+
+
+    ,
         editLink: {
-            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+            pattern: 'https://github.com/Jansora/doc-database/edit/main/:path',
             text: 'Edit this page on GitHub'
         },
         footer: {
-            message: 'Jansora Knowledge base',
-            copyright: 'Copyright © 2019-present Jansora'
+            message: 'Jansora\'s knowledge base',
+                copyright: 'Copyright © 2019-present Jansora'
         }
     }
 }
